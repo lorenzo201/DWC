41131bce568727c66a7896e9e611a2a69d80a6cf
lorenzo201 <31994257+lorenzo201@users.noreply.github.com>
2017-09-22 18:12:31 +0200

    Unidad 1Actividades

---
+++ Practica2/js1/U2Ej3script.js
@@ -0,0 +1,3 @@
+"Las rosas son rojas.\n
+\tEl mar es azul. \n
+\t\tNo se rimar."
--- Practica2/js1/mensaje1.js
+++ Practica2/js1/mensaje1.js
@@ -1,3 +1,5 @@
 alert(t);
+console.log("Esto es una cadena");
+console.info("Esto es info");
 console.warn("Esto es un aviso");
-console.error("Esto esun error");
+console.error("Esto es un error");
